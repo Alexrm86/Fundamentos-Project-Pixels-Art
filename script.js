c9a4b1eec07e630e4b09068bdb03f4960cb7bc16
Alex Resende Machado <alex.magal@gmail.com>
2022-03-21 17:10:43 -0300

    sexto requisito

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ function NumberFunc() {
     let Number = Math.floor(Math.random() * 255);
     return Number;
 }
+
+function Colors() {
+    let red = NumberFunc();
+    let green = NumberFunc();
+    let blue = NumberFunc();
+    let rgb = `rgb(${red}, ${green}, ${blue})`;
+    return rgb;
+}
